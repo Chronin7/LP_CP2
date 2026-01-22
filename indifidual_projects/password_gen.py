0d8f9d8532fdd8fa9a5971c0842abc797b68d077
Chronin7 <liam.perl@ucas-edu.net>
2026-01-22 09:51:46 -0700

    working on sudo code

---
+++ indifidual_projects/password_gen.py
@@ -0,0 +1,18 @@
+#import utill fucntions
+#symbols=33-47,58-64,91-96,123-126
+#digits=48-57
+#upercase=65-90
+#lowwercase=97-122
+#define main()
+#   reapete forever
+#       if util input do you want to use=n
+#           return
+#       leng=util input How long does the password need to be: 
+#       lowwerc=util input Does the password need lowercase letters (Y/N): 
+#       upperc=util input Does the password need uppercase letters (Y/N): 
+#       nums=util input Does the password need numbers (Y/N): 
+#       symbol=util input Does the password need special characters letters (Y/N):
+#       reapete 4 times
+#           password_set={}
+#           
+#           password_set add()
