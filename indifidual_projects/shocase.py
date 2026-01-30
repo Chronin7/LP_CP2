37622eef19d62a10485be553c03e33371862d2e2
Allen Perl <allen.perl@narvar.com>
2026-01-29 20:45:05 -0700

    ee

--- indifidual_projects/shocase.py
+++ indifidual_projects/shocase.py
@@ -9,20 +9,20 @@ def import_files(folder_path):
     global imported_modules
     sys.path.insert(0,folder_path)
     for filename in os.listdir(folder_path):
-        if filename[-3:]==".py" and filename !="shocase.py" and filename != "utill_functions.py":
-            module_name=filename[:-3]
-            module_names.append(filename[:-3])
-            try:
-                spec = importlib.util.spec_from_file_location(module_name, os.path.join(folder_path, filename))
-                module = importlib.util.module_from_spec(spec)
-                sys.modules[module_name] = module
-                spec.loader.exec_module(module)
-                imported_modules[module_name] = module
-            except Exception as e:
-                if filename!="utill_functions":
-                    print(f"Error importing {filename}: {e}")
-                else:
-                    pass
+        if filename[-3:]!=".py":
+            continue
+        module_name=filename[:-3]
+        if module_name in ["shocase","utill_functions"]:
+            continue
+        try:
+            spec = importlib.util.spec_from_file_location(module_name,os.path.join(folder_path,filename))
+            module = importlib.util.module_from_spec(spec)
+            sys.modules[module_name]=module
+            spec.loader.exec_module(module)
+            imported_modules[module_name]=module
+            module_names.append(module_name)
+        except Exception as e:
+            print(f"Error importing {filename}: {e}")
     sys.path.pop(0)
     return imported_modules, module_names
 mods,names=import_files('indifidual_projects')
@@ -35,3 +35,4 @@ while True:
         print("goodbye")
         break
     mods[names[user_input-1]].main()
+    
