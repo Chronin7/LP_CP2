af626b49286290f93ffda1ac83f59344dc1c0d67
Chronin7 <liam.perl@ucas-edu.net>
2026-01-30 09:42:46 -0700

    e

--- indifidual_projects/number_gessing_game.py
+++ indifidual_projects/number_gessing_game.py
@@ -1,32 +1,31 @@
 import random
+import utill_functions
 def main():
 		easterEggCount = 15
 		maxGuessCount = 20
 		minGuess = 1
 		maxGuess = 100
 		playCount = 0
-		playAgain = True
-		while playAgain == True:
-				if playCount == easterEggCount:
-						print("WHY DID YOU WASTE YOUR TIME ON THIS DUMB GAME! DO SOMETHING BETTER WITH YOUR TIME!")
-						quit()
-				playCount += 1
-				num = random.randint (minGuess,maxGuess)
-				print(f'Welcome the GUESS THE NUMBER! You have {maxGuessCount} attempts before you lose the game. good luck.')
-				guess = int(input(f'Guess a number {minGuess}-{maxGuess}: '))  
-				for x in range(maxGuessCount): 
-					if guess < num:
-						guess = int(input("the number is larger: "))
-					if guess > num:
-						guess = int(input("the number is smaller: "))
-					if guess == num : 
-						print ("you got it")
-						playgain = str(input("do you want to play again? (y/n): "))
-						if playgain != "y":
-							playAgain = False
-							print("ok sending you back to the hub")
-							return
-						else:
-							print("ok")
+		while True:
+			if playCount == easterEggCount:
+					print("WHY DID YOU WASTE YOUR TIME ON THIS DUMB GAME! DO SOMETHING BETTER WITH YOUR TIME!")
+					quit()
+			playCount += 1
+			num = random.randint (minGuess,maxGuess)
+			print(f'Welcome the GUESS THE NUMBER! You have {maxGuessCount} attempts before you lose the game. good luck.')
+			guess = utill_functions.get_valid_type(int,f'Guess a number {minGuess}-{maxGuess}: ',"that is not a number") 
+			for _ in range(maxGuessCount): 
+				if guess < num:
+					guess = int(input("the number is larger: "))
+				if guess > num:
+					guess = int(input("the number is smaller: "))
+				if guess == num : 
+					print ("you got it")	
+					playgain = str(input("do you want to play again? (y/n): "))
+					if playgain.lower() == "n":
+						return
+					else:
+						print("ok")
+							
 if __name__=="__main__":
     main()
