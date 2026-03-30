f71bd9b825e0f6da3f988e6ca090410b7795ca3c
Chronin7 <liam.perl@ucas-edu.net>
2026-03-30 09:53:00 -0600

    ai time

---
+++ personal_projects/ai_testing/helper.py
@@ -0,0 +1,11 @@
+import math,random
+class math_functions:
+    def __init__(self,function):
+        self.operation=function[0]
+        self.number=function[1]
+    def return_mod(self,prev):
+        if self.operation=="-":
+            return prev-self.number
+        elif self.operation
+class node:
+    def __init__(self,destinations,math_function):
